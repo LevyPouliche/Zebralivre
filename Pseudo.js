c64dfd0977a380011e20a9147e8c584cd2ee1923
LevyPouliche <caribouya23@gmail.com>
2026-02-05 12:38:11 -0500

    Delete Pseudo.js

--- Pseudo.js
+++
@@ -1,8 +0,0 @@
-const pseudo = localStorage.getItem("userPseudo");
-  if (pseudo) {
-    document.getElementById("nav-pseudo").textContent = pseudo;
-    document.getElementById("profile-pseudo").textContent = pseudo;
-  } else {
-    document.getElementById("nav-pseudo").textContent = "Aucun pseudo";
-    document.getElementById("profile-pseudo").textContent = "Aucun pseudo";
-  }
